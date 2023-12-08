5a53568e52287fc82fa7ac226fe1523efc5b2102
MarcLeMarco <marc.sabattier@etu.univ-lyon1.fr>
2023-12-08 14:39:36 +0100

    Database creation script

---
+++ src/dbCreation.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+conn = sqlite3.connect('/')
+c = conn.cursor()
+
+c.execute('''
+    DROP TABLE artist;
+    DROP TABLE albums;
+    DROP TABLE tunes;
+    DROP TABLE creates;
+''')
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS artists (
+        artistID INTEGER PRIMARY KEY,
+        name TEXT,
+        note REAL
+    )
+''')
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS albums (
+        albumID INTEGER PRIMARY KEY,
+        title TEXT,
+        releaseDate TEXT,
+        note REAL,
+        review TEXT   
+    )
+''')
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS tunes (
+        tuneID INTEGER PRIMARY KEY,
+        albumID INTEGER,
+        title TEXT,
+        note REAL,
+        review TEXT   
+    )
+''')
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS albums (
+        artistID INTEGER,
+        albumID INTEGER,
+        PRIMARY KEY (
+            artistID, albumID
+        )  
+    )
+''')
